7b46dd90daa9c94173e67d2aa5ba2e971b7e660a
Stephen Flowers <stephen.flowers@avidgr.com>
2019-11-18 22:27:59 +0000

    Add pygame stuff

---
+++ pygame_animation.py
@@ -0,0 +1,83 @@
+import pygame, sys, time
+from pygame.locals import *
+
+pygame.init()
+
+WINDOWWIDTH = 800
+WINDOWHEIGHT = 800
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation')
+
+DOWNLEFT = 'downleft'
+DOWNRIGHT = 'downright'
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'
+MOVESPEED = 4
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+b1 = {'rect':pygame.Rect(300, 80, 50, 100), 'color':RED, 'dir':UPRIGHT}
+b2 = {'rect':pygame.Rect(200, 200, 20, 20), 'color':GREEN, 'dir':UPLEFT}
+b3 = {'rect':pygame.Rect(100, 150, 60, 60), 'color':BLUE, 'dir':DOWNLEFT}
+boxes = [b1, b2, b3]
+
+#game loop
+while True:
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+			
+	windowSurface.fill(WHITE)
+	
+	for b in boxes:
+		if b['dir'] == DOWNLEFT:
+			b['rect'].left -= MOVESPEED
+			b['rect'].top += MOVESPEED
+		if b['dir'] == DOWNRIGHT:
+			b['rect'].left += MOVESPEED
+			b['rect'].top += MOVESPEED
+		if b['dir'] == UPLEFT:
+			b['rect'].left -= MOVESPEED
+			b['rect'].top -= MOVESPEED
+		if b['dir'] == UPRIGHT:
+			b['rect'].left += MOVESPEED
+			b['rect'].top -= MOVESPEED
+			
+		#check if the box has moved out of the window
+		if b['rect'].top < 0:
+			if b['dir'] == UPLEFT:
+				b['dir'] = DOWNLEFT
+			if b['dir'] == UPRIGHT:
+				b['dir'] = DOWNRIGHT
+		
+		#check if the box has moved out of the window
+		if b['rect'].bottom > WINDOWHEIGHT:
+			if b['dir'] == DOWNLEFT:
+				b['dir'] = UPLEFT
+			if b['dir'] == DOWNRIGHT:
+				b['dir'] = UPRIGHT
+				
+		#check if the box has moved out of the window
+		if b['rect'].left < 0:
+			if b['dir'] == DOWNLEFT:
+				b['dir'] = DOWNRIGHT
+			if b['dir'] == UPLEFT:
+				b['dir'] = UPRIGHT
+				
+		#check if the box has moved out of the window
+		if b['rect'].right > WINDOWWIDTH:
+			if b['dir'] == DOWNRIGHT:
+				b['dir'] = DOWNLEFT
+			if b['dir'] == UPRIGHT:
+				b['dir'] = UPLEFT
+				
+		pygame.draw.rect(windowSurface, b['color'], b['rect'])
+		
+	pygame.display.update()
+	time.sleep(0.02)
+	
+			
